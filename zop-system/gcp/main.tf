51ce15c2d4f68ab236fc2d125d0432bdcef4ab2a
PiyushSingh-ZS <125456748+PiyushSingh-ZS@users.noreply.github.com>
2024-11-18 03:11:35 +0530

    Development (#11)
    * update kube-management-api image tag
    
    * fix azure host and badger db access modes
    
    * update kube-management-api version to v0.0.2
    
    * update zop helm
    
    * update kube-management-api to version v0.0.3
    
    * fix azure zop-system
    
    * update kube management api
    
    * update kube management api

--- zop-system/gcp/main.tf
+++ zop-system/gcp/main.tf
@@ -16,7 +16,7 @@ resource "helm_release" "service_helm"{
   name        = "kube-management-api"
   namespace   = "zop-system"
   repository  = "https://helm.zop.dev"
-  version     = "v0.0.2"
+  version     = "v0.0.4"
   chart       = "service"
 
   set {
@@ -26,7 +26,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-gcp:v0.0.3"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-gcp:v0.0.4"
   }
 
   set_list {
