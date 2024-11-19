4ba3e33fe1f587caabf9cd6433e60741595457c6
PiyushSingh-ZS <piyush.singh@zop.dev>
2024-11-20 01:32:35 +0530

    update kube-management-api

--- zop-system/gcp/main.tf
+++ zop-system/gcp/main.tf
@@ -26,7 +26,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-gcp:729890ee850f17db5aeba2bcb43725b87d3bc364"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-gcp:b5945d1a7568ed084685ffec382d222488652d1f"
   }
 
   set_list {
