7685acf45e2164b519a5877b244ed30a409e8df4
PiyushSingh-ZS <125456748+PiyushSingh-ZS@users.noreply.github.com>
2024-11-22 18:19:36 +0530

    Development (#16)
    * update kube-management-api v0.0.8

--- zop-helm/service/main.tf
+++ zop-helm/service/main.tf
@@ -13,7 +13,7 @@ resource "helm_release" "service_helm"{
   name        = var.name
   namespace   = var.namespace
   repository  = "https://helm.zop.dev"
-  version     = "v0.0.5"
+  version     = "v0.0.6"
   chart       = "service"
   reuse_values = true
 
