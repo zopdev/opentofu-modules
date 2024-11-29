97c025e9ccdc1fc2a94d6afdf24d4f96b3e37150
PiyushSingh-ZS <piyush.singh@zop.dev>
2024-11-29 17:36:44 +0530

    update kube management api v0.0.10

--- zop-system/azure/main.tf
+++ zop-system/azure/main.tf
@@ -24,7 +24,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-azure:v0.0.9"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-azure:v0.0.10"
   }
 
   set_list {
