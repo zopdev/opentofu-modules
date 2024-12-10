7f7f82447ac7a19a10eacf91fa49ecb1690cb24d
PiyushSingh-ZS <piyush.singh@zop.dev>
2024-12-10 12:05:20 +0530

    update kube-management-pai

--- zop-system/aws/main.tf
+++ zop-system/aws/main.tf
@@ -24,7 +24,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-aws:v0.0.10"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-aws:5c8e7deb9954f66687ca3815dc7aae6028f5bd1b"
   }
 
   set_list {
