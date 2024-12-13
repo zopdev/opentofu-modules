993f00eb1fb41f61decb8c1d0c953caeda2d826e
PiyushSingh-ZS <piyush.singh@zop.dev>
2024-12-13 19:12:37 +0530

    update helm chart versions

--- zop-helm/cronjob/main.tf
+++ zop-helm/cronjob/main.tf
@@ -13,7 +13,7 @@ resource "helm_release" "cron_helm"{
   name        = var.name
   namespace   = var.namespace
   repository  = "https://helm.zop.dev"
-  version     = "v0.0.6"
+  version     = "v0.0.7"
   chart       = "cron-job"
   reuse_values = true
 
--- zop-system/aws/main.tf
+++ zop-system/aws/main.tf
@@ -14,7 +14,7 @@ resource "helm_release" "service_helm"{
   name        = "kube-management-api"
   namespace   = "zop-system"
   repository  = "https://helm.zop.dev"
-  version     = "v0.0.7"
+  version     = "v0.0.8"
   chart       = "service"
 
   set {
@@ -24,7 +24,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-aws:v0.0.9"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-aws:1a26ccbc4360f2b2b88596699d076f193737156e"
   }
 
   set_list {
