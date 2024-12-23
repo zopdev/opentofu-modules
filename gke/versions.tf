7465fcb626a8a4296e810e5f3037506daad7a537
Arunesh J <arunesh.j@zop.dev>
2024-12-23 12:52:15 +0530

    update gke cluster module version

--- gke/versions.tf
+++ gke/versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 5.0.0, < 6"
+      version = ">= 5.44.0, < 6"
     }
     kubernetes = {
       source  = "hashicorp/kubernetes"
