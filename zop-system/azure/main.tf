2a785ffb3b2bce45b2cd6a6438d5a3e29dc3082b
Arunesh J <arunesh.j@zop.dev>
2024-12-27 17:38:39 +0530

    use opentofu development

--- zop-system/azure/main.tf
+++ zop-system/azure/main.tf
@@ -14,7 +14,7 @@ resource "helm_release" "service_helm"{
   name        = "kube-management-api"
   namespace   = "zop-system"
   repository  = "https://helm.zop.dev"
-  version     = "v0.0.10"
+  version     = "v0.0.12"
   chart       = "service"
 
   set {
@@ -24,7 +24,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-azure:v0.0.14"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-azure:8590ef902e3be89006582c7fe5ddaf8362537263"
   }
 
   set_list {
