233c31d748d06f75de93bffe7fe159cc6875c96c
Arunesh J <arunesh.j@zop.dev>
2025-02-26 19:25:06 +0530

    Update Kube Management Version  (#64)

--- zop-system/aws/main.tf
+++ zop-system/aws/main.tf
@@ -24,7 +24,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-aws:v0.0.19"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-aws:v0.0.20"
   }
 
   set_list {
