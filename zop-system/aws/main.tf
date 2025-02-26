1bc6e09b8638e438a574d6e76ce1d20775313d6e
Arunesh J <arunesh.j@zop.dev>
2025-02-26 19:13:45 +0530

    use kube management v0.0.20

--- zop-system/aws/main.tf
+++ zop-system/aws/main.tf
@@ -24,7 +24,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-aws:2e4606329b57b6d69faf6342bf371a9258021941"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-aws:v0.0.20"
   }
 
   set_list {
