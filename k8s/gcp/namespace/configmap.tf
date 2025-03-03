351bd520fefa237d6dd38b6568742b075865f191
Arunesh J <arunesh.j@zop.dev>
2025-03-03 11:01:50 +0530

    update gcp redis configs

--- k8s/gcp/namespace/configmap.tf
+++ k8s/gcp/namespace/configmap.tf
@@ -33,19 +33,8 @@ resource "kubernetes_config_map" "service_configs" {
       "DB_DIALECT"      = each.value.db_name != null ? module.sql_db[0].db_type :  each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_type : null
       "DB_HOST"         = each.value.db_name != null ? "${var.namespace}-sql.db" : each.value.datastore_configs != null ?  "${each.value.datastore_configs.name}-sql.db" : null
       "DB_PORT"         = each.value.db_name != null ? module.sql_db[0].db_port : each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_port : null
-      "REDIS_HOST" = (
-        each.value.redis == true || each.value.local_redis == true
-        ? (each.value.redis == true ? "${var.namespace}-redis" : "redis-master-master")
-        : (try(each.value.redis_configs.name, null) != null
-        ? "${each.value.redis_configs.name}-${var.namespace}-redis"
-        : null)
-      ),
-
-      "REDIS_PORT" = (
-        each.value.redis == true || each.value.local_redis == true
-        ? "6379"
-        : try(each.value.redis_configs.port, null)
-      ),
+      "REDIS_HOST"      = each.value.redis == true || each.value.local_redis == true ? (each.value.redis == true ? "${var.namespace}-redis" : "redis-master-master") : (each.value.redis_configs != null ? "${each.value.redis_configs.name}-${var.namespace}-redis" : null),
+      "REDIS_PORT"      = each.value.redis == true || each.value.local_redis == true ? "6379" : (each.value.redis_configs != null ? each.value.redis_configs.port : null)
     })
 }
 
@@ -66,19 +55,8 @@ resource "kubernetes_config_map" "cron_jobs_configs" {
       "DB_DIALECT"      = each.value.db_name != null ? module.sql_db[0].db_type :  each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_type : null
       "DB_HOST"         = each.value.db_name != null ? "${var.namespace}-sql.db" : each.value.datastore_configs != null ?  "${each.value.datastore_configs.name}-sql.db" : null
       "DB_PORT"         = each.value.db_name != null ? module.sql_db[0].db_port : each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_port : null
-      "REDIS_HOST" = (
-        each.value.redis == true || each.value.local_redis == true
-        ? (each.value.redis == true ? "${var.namespace}-redis" : "redis-master-master")
-        : (try(each.value.redis_configs.name, null) != null
-        ? "${each.value.redis_configs.name}-${var.namespace}-redis"
-        : null)
-      ),
-
-      "REDIS_PORT" = (
-        each.value.redis == true || each.value.local_redis == true
-        ? "6379"
-        : try(each.value.redis_configs.port, "6379")
-      ),
+      "REDIS_HOST"      = each.value.redis == true || each.value.local_redis == true ? (each.value.redis == true ? "${var.namespace}-redis" : "redis-master-master") : (each.value.redis_configs != null ? "${each.value.redis_configs.name}-${var.namespace}-redis" : null),
+      "REDIS_PORT"      = each.value.redis == true || each.value.local_redis == true ? "6379" : (each.value.redis_configs != null ? each.value.redis_configs.port : null)
     })
 }
 
