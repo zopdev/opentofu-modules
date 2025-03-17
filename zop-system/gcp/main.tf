22552a8d807b45eeb716ae6595c9a88863624b59
Arunesh J <arunesh.j@zop.dev>
2025-03-17 12:39:16 +0530

    update kube version

--- zop-system/gcp/main.tf
+++ zop-system/gcp/main.tf
@@ -26,7 +26,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-gcp:v0.0.21"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-gcp:522bc0ec9c4980575087df2fa9da192dcc7bb256"
   }
 
   set_list {
