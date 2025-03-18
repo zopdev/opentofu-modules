2f7ce7145bf14d4eddd7a8aadd3461621d8cecc8
Arunesh J <arunesh.j@zop.dev>
2025-03-18 11:36:55 +0530

    update kube-management-api to v0.0.22 (#82)

--- zop-system/azure/main.tf
+++ zop-system/azure/main.tf
@@ -24,7 +24,7 @@ resource "helm_release" "service_helm"{
 
   set {
     name  = "image"
-    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-azure:v0.0.21"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kube-management-api-azure:v0.0.22"
   }
 
   set_list {
