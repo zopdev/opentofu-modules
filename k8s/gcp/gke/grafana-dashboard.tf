d6074f2ed28fc633edf9055f3e1b21feb2dcdd36
Arunesh J <arunesh.j@zop.dev>
2025-04-09 11:43:29 +0530

    update gcp grafana permission

--- k8s/gcp/gke/grafana-dashboard.tf
+++ k8s/gcp/gke/grafana-dashboard.tf
@@ -25,6 +25,57 @@ locals {
   ]...)
 }
 
+resource "random_password" "admin_passwords" {
+  for_each = coalesce(toset(var.user_access.app_admins), toset([]))
+  length   = 16
+  special  = true
+}
+
+resource "random_password" "editor_passwords" {
+  for_each = coalesce(toset(var.user_access.app_editors), toset([]))
+  length   = 16
+  special  = true
+}
+
+resource "random_password" "viewer_passwords" {
+  for_each = coalesce(toset(var.user_access.app_viewers), toset([]))
+  length   = 16
+  special  = true
+}
+
+resource "grafana_user" "admins" {
+  for_each = coalesce(toset(var.user_access.app_admins), toset([]))
+  name     = split("@", each.key)[0]
+  email    = each.key
+  login    = split("@", each.key)[0]
+  password = random_password.admin_passwords[each.key].result
+  is_admin = true
+
+  depends_on = [helm_release.grafana]
+}
+
+resource "grafana_user" "editors" {
+  for_each = coalesce(toset(var.user_access.app_editors), toset([]))
+  name     = split("@", each.key)[0]
+  email    = each.key
+  login    = split("@", each.key)[0]
+  password = random_password.editor_passwords[each.key].result
+  is_admin = false
+
+  depends_on = [helm_release.grafana]
+}
+
+resource "grafana_user" "viewers" {
+  for_each = coalesce(toset(var.user_access.app_viewers), toset([]))
+  name     = split("@", each.key)[0]
+  email    = each.key
+  login    = split("@", each.key)[0]
+  password = random_password.viewer_passwords[each.key].result
+  is_admin = false
+
+  depends_on = [helm_release.grafana]
+}
+
 resource "grafana_folder" "dashboard_folder" {
   for_each   = { for obj in local.folder_map : obj.folder => obj }
   title      = each.key
