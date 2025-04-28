a610c93bb85e47fd0131b17ffab040e784671562
PiyushSingh-ZS <piyush.singh@zop.dev>
2025-04-29 01:48:45 +0530

    fix cronjob image update

--- k8s/azure/namespace/cronjob.tf
+++ k8s/azure/namespace/cronjob.tf
@@ -15,7 +15,7 @@ locals {
       (v.helm_configs != null && v.helm_configs.image != null && v.helm_configs.image != "")
       ? v.helm_configs.image
       : "zopdev/sample-go-api:latest"
-    ) if (!contains(keys(local.deployment_api_image_map), k))
+    ) if (!contains(keys(local.cron_api_image_map), k))
   })
 
   cron_all_images = merge(local.cron_existing_images, local.cron_new_images)
