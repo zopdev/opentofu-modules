d305548fc9e9a566d098b10c9218a2114089bb81
Arunesh J <arunesh.j@zop.dev>
2025-04-29 13:06:17 +0530

    add namespace module

---
+++ k8s/oci/namespace/kubernetes.tf
@@ -0,0 +1,71 @@
+locals {
+  cluster_prefix = var.shared_services.cluster_prefix != null ? var.shared_services.cluster_prefix : var.app_name
+}
+
+module "remote_state_gcp_cluster" {
+  source         = "../../../remote-state/gcp"
+  count          = var.shared_services.type == "gcp" ? 1 : 0
+  bucket_name    = var.shared_services.bucket
+  bucket_prefix  = local.cluster_prefix
+}
+
+module "remote_state_aws_cluster" {
+  source         = "../../../remote-state/aws"
+  count          = var.shared_services.type == "aws" ? 1 : 0
+  bucket_name    = var.shared_services.bucket
+  provider_id    = var.shared_services.profile
+  bucket_prefix  = local.cluster_prefix
+  location       = var.shared_services.location
+}
+
+module "remote_state_azure_cluster" {
+  source          = "../../../remote-state/azure"
+  count           = var.shared_services.type == "azure" ? 1 : 0
+  resource_group  = var.shared_services.resource_group
+  storage_account = var.shared_services.storage_account
+  container       = var.shared_services.container
+  bucket_prefix   = local.cluster_prefix
+}
+
+module "remote_state_oci_cluster" {
+  source          = "../../../remote-state/oci"
+  count           = var.shared_services.type == "oci" ? 1 : 0
+  bucket_name     = var.shared_services.bucket
+  bucket_prefix   = local.cluster_prefix
+}
+
+provider "kubernetes" {
+  host                   = "https://${module.remote_state_oci_cluster.0.kubernetes_endpoint.public_endpoint}"
+  cluster_ca_certificate = base64decode(module.remote_state_oci_cluster.0.ca_certificate)
+
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "oci"
+    args        = ["ce", "cluster", "generate-token", "--cluster-id", module.remote_state_oci_cluster.0.cluster_id, "--region", var.app_region]
+  }
+}
+
+provider "helm" {
+  kubernetes {
+    host                   = "https://${module.remote_state_oci_cluster.0.kubernetes_endpoint.public_endpoint}"
+    cluster_ca_certificate = base64decode(module.remote_state_oci_cluster.0.ca_certificate)
+    
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "oci"
+      args        = ["ce", "cluster", "generate-token", "--cluster-id", module.remote_state_oci_cluster.0.cluster_id, "--region", var.app_region]
+    }
+  }
+}
+
+provider "kubectl" {
+  host                   = "https://${module.remote_state_oci_cluster.0.kubernetes_endpoint.public_endpoint}"
+  cluster_ca_certificate = base64decode(module.remote_state_oci_cluster.0.ca_certificate)
+  
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "oci"
+    args        = ["ce", "cluster", "generate-token", "--cluster-id", module.remote_state_oci_cluster.0.cluster_id, "--region", var.app_region]
+  }
+  load_config_file = false
+}
