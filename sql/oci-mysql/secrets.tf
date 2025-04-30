5ed4089a4bd68e1ecb0dd5b03fba7eb1119f22d6
Arunesh J <arunesh.j@zop.dev>
2025-04-30 13:04:35 +0530

    add oci sql modules

---
+++ sql/oci-mysql/secrets.tf
@@ -0,0 +1,120 @@
+
+locals {
+  db_map = tomap({
+    for database in var.databases : "${var.namespace}-${database}" => {
+      master_db = var.namespace
+      db_name   = database
+      user      = "${database}_${random_string.mysql_username[database].result}"
+    } if database != null
+  })
+}
+
+resource "random_password" "db_admin_password" {
+  length           = 16
+  special          = true
+  override_special = "!@$_+-=?"
+  upper            = true
+  lower            = true
+  number           = true
+}
+
+resource "random_string" "mysql_username" {
+  length   = 6
+  special  = false
+  for_each = {for v in var.databases : v => v if v != null}
+}
+
+resource "random_password" "mysql_editor_password" {
+  length           = 16   
+  special          = true
+  upper            = true
+  lower            = true
+  numeric          = true
+  min_special      = 2    
+  min_upper        = 2    
+  min_lower        = 2    
+  min_numeric      = 2   
+  for_each         = local.db_map
+}
+
+resource "random_password" "mysql_reader_password" {
+  length           = 16   
+  special          = true
+  upper            = true
+  lower            = true
+  numeric          = true
+  min_special      = 2    
+  min_upper        = 2    
+  min_lower        = 2    
+  min_numeric      = 2   
+  for_each         = local.db_map
+}
+
+resource "oci_vault_secret" "admin_password_secret" {
+  compartment_id = var.provider_id
+  secret_name    = "${var.mysql_db_system_name}-mysql-db-secret"
+  secret_content {
+     content_type = "base64"
+     content = base64encode(random_password.db_admin_password.result)
+  }
+  key_id         = var.key_id
+  vault_id       = var.vault_id
+}
+
+resource "oci_vault_secret" "mysql_db_user_secret" {
+  for_each        = local.db_map
+  compartment_id  = var.provider_id
+  secret_name     = "${var.mysql_db_system_name}-${replace(each.key, "_", "-")}-mysql-db-user"
+  vault_id        = var.vault_id
+  key_id          = var.key_id
+
+  secret_content {
+    content_type = "BASE64"
+    content      = base64encode(each.value.user)
+  }
+}
+
+resource "oci_vault_secret" "mysql_db_editor_secret" {
+  for_each        = local.db_map
+  compartment_id  = var.provider_id
+  secret_name     = "${var.mysql_db_system_name}-${replace(each.key, "_", "-")}-mysql-db"
+  vault_id        = var.vault_id
+  key_id          = var.key_id
+
+  secret_content {
+    content_type = "BASE64"
+    content      = base64encode(random_password.mysql_editor_password[each.key].result)
+  }
+}
+
+resource "oci_vault_secret" "mysql_db_readonly_secret" {
+  for_each        = local.db_map
+  compartment_id  = var.provider_id
+  secret_name     = "${var.mysql_db_system_name}-${replace(each.key, "_", "-")}-mysql-readonly"
+  vault_id        = var.vault_id
+  key_id          = var.key_id
+
+  secret_content {
+    content_type = "BASE64"
+    content      = base64encode(random_password.mysql_reader_password[each.key].result)
+  }
+}
+
+data "oci_secrets_secretbundle" "admin_password_bundle" {
+  secret_id = oci_vault_secret.admin_password_secret.id
+}
+
+data "oci_secrets_secretbundle" "mysql_db_user_secret" {
+  for_each  = local.db_map
+  secret_id = oci_vault_secret.mysql_db_user_secret[each.key].id
+}
+
+data "oci_secrets_secretbundle" "mysql_db_editor_secret" {
+  for_each  = local.db_map
+  secret_id = oci_vault_secret.mysql_db_editor_secret[each.key].id
+}
+
+data "oci_secrets_secretbundle" "mysql_db_readonly_secret" {
+  for_each  = local.db_map
+  secret_id = oci_vault_secret.mysql_db_readonly_secret[each.key].id
+}
