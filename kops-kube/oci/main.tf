e471d5488f412ef1747a7b6898c37a2a84a3dad7
Arunesh J <arunesh.j@zop.dev>
2025-05-20 17:19:48 +0530

    ADD : kops-kube module for oci(#143)

---
+++ kops-kube/oci/main.tf
@@ -0,0 +1,147 @@
+resource "helm_release" "service_helm"{
+  name        = "kops-kube"
+  namespace   = "kube-system"
+  repository  = "https://helm.zop.dev"
+  version     = "v0.0.2"
+  chart       = "service"
+
+  set {
+    name  = "name"
+    value = "kops-kube"
+  }
+
+  set {
+    name  = "image"
+    value = "us-central1-docker.pkg.dev/raramuri-tech/kops-dev/kops-kube-oci:5290f0fd14828507e2d3e5c82e37cd636dc6961d"
+  }
+
+  set_list {
+    name = "imagePullSecrets"
+    value = ["kops-kube-image-secrets"]
+  }
+
+  set {
+    name  = "replicaCount"
+    value = 1
+  }
+
+  set {
+    name  = "httpPort"
+    value = 8000
+  }
+
+  set {
+    name  = "metricsPort"
+    value = 2121
+  }
+
+  set {
+    name  = "minCPU"
+    value = "100m"
+  }
+
+  set {
+    name  = "minMemory"
+    value = "100M"
+  }
+
+  set {
+    name  = "maxCPU"
+    value = "200m"
+  }
+
+  set {
+    name  = "maxMemory"
+    value = "500M"
+  }
+
+  set {
+    name  = "minAvailable"
+    value = 1
+  }
+
+  set {
+    name  = "hpa_enable"
+    value = true
+  }
+
+  set {
+    name  = "minReplicas"
+    value = 1
+  }
+
+  set {
+    name  = "maxReplicas"
+    value = 4
+  }
+
+  set {
+    name  = "heartbeatURL"
+    value = "./well-known/alive"
+  }
+
+  set_list {
+    name = "envFrom.secrets"
+    value = ["kops-kube-secret"]
+  }
+
+  values = [templatefile("./templates/values.yaml",{
+    cluster_name    = var.cluster_name
+    app_region      = var.app_region
+    cloud_platform  = "OCI"
+    provider_id     = var.provider_id
+    cluster_ocid    = data.oci_containerengine_clusters.oke.clusters[0].id
+    provider_name   = "DEFAULT"
+  })]
+}
+
+resource "kubernetes_ingress_v1" "kops_kube_ingress" {
+  metadata {
+    name      = "kops-kube-ingress"
+    namespace = "kube-system"
+    annotations = {
+      "kubernetes.io/ingress.class" = "nginx"
+    }
+  }
+  spec {
+    rule {
+      host = "kops-kube.${var.host}"
+      http {
+        path {
+          backend {
+            service {
+              name = "kops-kube"
+              port {
+                number = 8000
+              }
+            }
+          }
+          path = "/"
+        }
+      }
+    }
+    tls {
+      secret_name = "tls-secret-replica"
+      hosts       = ["*.${var.host}"]
+    }
+  }
+  depends_on = [kubernetes_secret_v1.namespace-cert-replicator]
+}
+
+resource "kubernetes_secret_v1" "namespace-cert-replicator" {
+  metadata {
+    name = "tls-secret-replica"
+    namespace = "kube-system"
+    annotations = {
+      "replicator.v1.mittwald.de/replicate-from" = "cert-manager/wildcard-dns"
+    }
+  }
+  type = "kubernetes.io/tls"
+  data = {
+    "tls.key" = ""
+    "tls.crt" = ""
+  }
+  lifecycle {
+    ignore_changes = all
+  }
+}
---
+++ kops-kube/oci/vars.tf
@@ -0,0 +1,33 @@
+variable "app_region" {
+  type = string
+  description = "Location where the resources to be created"
+  default = ""
+}
+
+variable "cluster_name" {
+  description = "Name of the cluster on which kube-management-api should be deployed"
+  type        = string
+}
+
+variable "provider_id" {
+  description = "Compartment ID"
+  type        = string
+}
+
+variable "host" {
+  description = "Domain to be used for kube-management-api"
+  type        = string
+}
+
+variable "shared_services" {
+  type = object({
+    type     = string
+    bucket   = string
+    profile  = optional(string)
+    location = optional(string)
+    resource_group = optional(string)
+    storage_account = optional(string)
+    container = optional(string)
+    cluster_prefix = optional(string)
+  })
+}
