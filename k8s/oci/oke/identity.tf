812d80313bff47d9086bcee9f5af44b865663f69
Arunesh J <arunesh.j@zop.dev>
2025-05-26 16:34:26 +0530

    update outputs

--- k8s/oci/oke/identity.tf
+++ k8s/oci/oke/identity.tf
@@ -18,7 +18,7 @@ locals {
     for email in local.input_user_emails : email => email
     if (
       !contains(keys(local.existing_oci_users_map), email) ||
-      endswith(lookup(lookup(local.existing_oci_users_map, email, {}), "name", ""))
+      endswith(lookup(lookup(local.existing_oci_users_map, email, {}), "name", ""), "-zop")
     )
   }
 
--- k8s/oci/oke/outputs.tf
+++ k8s/oci/oke/outputs.tf
@@ -81,20 +81,24 @@ output "grafana_host" {
   value = try(local.grafana_host,"")
 }
 
-# output "grafana_user_credentials" {
-#   value = merge(
-#     { for key, pwd in random_password.admin_passwords : key => {
-#       email    = key
-#       password = pwd.result
-#     }},
-#     { for key, pwd in random_password.editor_passwords : key => {
-#       email    = key
-#       password = pwd.result
-#     }},
-#     { for key, pwd in random_password.viewer_passwords : key => {
-#       email    = key
-#       password = pwd.result
-#     }}
-#   )
-#   sensitive = true
-# }
+output "managed_users" {
+  value = local.managed_users
+}
+
+output "grafana_user_credentials" {
+  value = merge(
+    { for key, pwd in random_password.admin_passwords : key => {
+      email    = key
+      password = pwd.result
+    }},
+    { for key, pwd in random_password.editor_passwords : key => {
+      email    = key
+      password = pwd.result
+    }},
+    { for key, pwd in random_password.viewer_passwords : key => {
+      email    = key
+      password = pwd.result
+    }}
+  )
+  sensitive = true
+}
