3ceee03dc23494c7c6f282f3b46c6ace36a937ec
Arunesh J <arunesh.j@zop.dev>
2025-05-28 23:24:45 +0530

    update outputs of namespace module

--- k8s/oci/namespace/output.tf
+++ k8s/oci/namespace/output.tf
@@ -34,13 +34,17 @@ output "sql_instance_configs" {
 output "service_configs" {
   value = {
     for k, v in var.services : k => {
-      db_name                               = v.datastore_configs != null && v.datastore_configs.database != null ? v.datastore_configs.database : ""
-      db_secret_name                        = v.datastore_configs != null && v.datastore_configs.database != null ? "${k}-${v.datastore_configs.database}-mysql-db-user" : ""
-      db_read_only_secret_name              = v.datastore_configs != null && v.datastore_configs.database != null ? "${k}-${v.datastore_configs.database}-mysql-readonly" : ""
-      db_user                               = v.datastore_configs != null && v.datastore_configs.database != null ? (
-                                                v.datastore_configs.type == "mysql" ? module.sql_db[0].db_user["${var.namespace}-${v.datastore_configs.database}"] :
-                                                (v.datastore_configs.type == "postgres" ? module.psql_db[0].db_user["${var.namespace}-${v.datastore_configs.database}"] : "")
-                                              ) : ""
+      db_name                              = try(v.datastore_configs.database, "")
+      db_secret_name                       = try("${k}-${v.datastore_configs.database}-mysql-db-user", "")
+      db_read_only_secret_name             = try("${k}-${v.datastore_configs.database}-mysql-readonly", "")
+      db_user                              = try(
+                                              v.datastore_configs.type == "mysql" ?
+                                                module.sql_db[0].db_user["${var.namespace}-${v.datastore_configs.database}"] :
+                                              (v.datastore_configs.type == "postgres" ?
+                                                module.psql_db[0].db_user["${var.namespace}-${v.datastore_configs.database}"] :
+                                                ""),
+                                              ""
+                                            )
       custom_host_url                       = v.ingress_list != null ? (length(v.ingress_list) != 0 ? v.ingress_list : []) : []
       default_host_url                      = v.enable_default_ingress != null ? (
                                                 v.enable_default_ingress ? kubernetes_ingress_v1.default_service_ingress["${k}-${var.namespace}-${local.default_domain_list[k].ingress[0]}"].spec[0].rule[0].host : ""
@@ -60,13 +64,17 @@ output "cron_jobs_configs" {
   value = {
     for k, v in var.cron_jobs : k =>
     {
-      db_name                               = v.datastore_configs != null && v.datastore_configs.database != null ? v.datastore_configs.database : ""
-      db_secret_name                        = v.datastore_configs != null && v.datastore_configs.database != null ? "${k}-${v.datastore_configs.database}-mysql-db-user" : ""
-      db_read_only_secret_name              = v.datastore_configs != null && v.datastore_configs.database != null ? "${k}-${v.datastore_configs.database}-mysql-readonly" : ""
-      db_user                               = v.datastore_configs != null && v.datastore_configs.database != null ? (
-                                                v.datastore_configs.type == "mysql" ? module.sql_db[0].db_user["${var.namespace}-${v.datastore_configs.database}"] :
-                                                (v.datastore_configs.type == "postgres" ? module.psql_db[0].db_user["${var.namespace}-${v.datastore_configs.database}"] : "")
-                                              ) : ""
+      db_name                              = try(v.datastore_configs.database, "")
+      db_secret_name                       = try("${k}-${v.datastore_configs.database}-mysql-db-user", "")
+      db_read_only_secret_name             = try("${k}-${v.datastore_configs.database}-mysql-readonly", "")
+      db_user                              = try(
+                                              v.datastore_configs.type == "mysql" ?
+                                                module.sql_db[0].db_user["${var.namespace}-${v.datastore_configs.database}"] :
+                                              (v.datastore_configs.type == "postgres" ?
+                                                module.psql_db[0].db_user["${var.namespace}-${v.datastore_configs.database}"] :
+                                                ""),
+                                              ""
+                                            )
       oar_username                          = "${data.oci_objectstorage_namespace.tenancy_namespace.namespace}/${var.namespace}-artifact-user"
       oar_password                          = oci_identity_auth_token.artifact_user_token.token
     }
