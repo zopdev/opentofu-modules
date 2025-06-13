9cafdf895cbf7f89e0761ae006c8ac465a9e6f2c
PiyushSingh-ZS <piyush.singh@zop.dev>
2025-06-13 09:53:19 +0530

    add kiali helm chart

--- k8s/gcp/gke/observability.tf
+++ k8s/gcp/gke/observability.tf
@@ -38,20 +38,25 @@ resource "helm_release" "kiali" {
   repository = "https://kiali.org/helm-charts"
   chart      = "kiali-server"
   namespace  = "istio-system"
-
+  
   set {
     name  = "auth.strategy"
     value = "anonymous"
   }
-
+  
   set {
     name  = "external_services.istio.root_namespace"
     value = "istio-system"
   }
-
+  
   set {
     name  = "external_services.istio.istiod_url"
     value = "http://istiod.istio-system:15012"
+  }
+
+  set {
+    name  = "external_services.prometheus.url"
+    value = "http://prometheus-kube-prometheus-prometheus.monitoring:9090"
   }
 
   set {
@@ -83,7 +88,7 @@ resource "helm_release" "kiali" {
     name  = "deployment.pod_annotations.kubernetes\\.io/egress-bandwidth"
     value = "10M"
   }
-
+  
   depends_on = [helm_release.istiod]
 }
 
@@ -93,68 +98,68 @@ resource "helm_release" "kiali" {
 #   repository = "https://jaegertracing.github.io/helm-charts"
 #   chart      = "jaeger"
 #   namespace  = "istio-system"
-#
+  
 #   set {
 #     name  = "query.service.type"
 #     value = "ClusterIP"
 #   }
-#
+  
 #   set {
 #     name  = "collector.service.type"
 #     value = "ClusterIP"
 #   }
-#
+
 #   set {
 #     name  = "query.resources.requests.memory"
 #     value = "128Mi"
 #   }
-#
+
 #   set {
 #     name  = "query.resources.requests.cpu"
 #     value = "100m"
 #   }
-#
+
 #   set {
 #     name  = "query.resources.limits.memory"
 #     value = "256Mi"
 #   }
-#
+
 #   set {
 #     name  = "query.resources.limits.cpu"
 #     value = "200m"
 #   }
-#
+
 #   set {
 #     name  = "collector.resources.requests.memory"
 #     value = "128Mi"
 #   }
-#
+
 #   set {
 #     name  = "collector.resources.requests.cpu"
 #     value = "100m"
 #   }
-#
+
 #   set {
 #     name  = "collector.resources.limits.memory"
 #     value = "256Mi"
 #   }
-#
+
 #   set {
 #     name  = "collector.resources.limits.cpu"
 #     value = "200m"
 #   }
-#
+
 #   set {
 #     name  = "storage.type"
 #     value = "memory"
 #   }
-#
+
 #   set {
 #     name  = "storage.options.memory.max-traces"
 #     value = "50000"
 #   }
-#
+  
 #   depends_on = [helm_release.istiod]
 # }
-#
-#
+
+
