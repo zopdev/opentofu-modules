14835dbde38f0e413726b29987e8e7d12b07d4f8
Arunesh J <arunesh.j@zop.dev>
2025-06-20 18:15:41 +0530

    fix comments

--- object-storage/aws/main.tf
+++ object-storage/aws/main.tf
@@ -1,11 +1,11 @@
-resource "aws_s3_bucket" "this" {
+resource "aws_s3_bucket" "s3_bucket" {
   for_each      = toset(var.bucket_names)
   bucket        = each.key
   force_destroy = true
 }
 
-resource "aws_s3_bucket_versioning" "this" {
-  for_each = aws_s3_bucket.this
+resource "aws_s3_bucket_versioning" "s3_versioning" {
+  for_each = aws_s3_bucket.s3_bucket
 
   bucket = each.value.id
   versioning_configuration {
