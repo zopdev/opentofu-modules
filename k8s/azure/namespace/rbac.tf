9cebf85fda3b44d600a00e87f5cdccfba7191c7c
gourish25 <gourish.hegde@zop.dev>
2025-07-28 17:36:51 +0530

    Adds RoleBinding to grant Kubernetes namespace editor permission to each service's Azure Service Principal

--- k8s/azure/namespace/rbac.tf
+++ k8s/azure/namespace/rbac.tf
@@ -46,3 +46,24 @@ resource "kubernetes_role_binding" "viewer" {
     name = azuread_group.aks_aad_namespace_viewers.object_id
   }
 }
+
+resource "kubernetes_role_binding" "service_principal_namespace_editor" {
+  for_each = var.services
+
+  metadata {
+    name      = "${each.key}-sp-edit-binding"
+    namespace = kubernetes_namespace.app_environments.metadata[0].name
+  }
+
+  role_ref {
+    api_group = "rbac.authorization.k8s.io"
+    kind      = "ClusterRole"
+    name      = "edit"
+  }
+
+  subject {
+    kind      = "User"
+    name      = azuread_service_principal.acr_sp[each.key].object_id
+    api_group = "rbac.authorization.k8s.io"
+  }
+}
