02fab029547ab7fdb0668f378da7344ef0aea4bb
arunesh-j <arunesh.j@zop.dev>
2025-08-13 12:48:50 +0530

    update hashicorp aws version

--- account-setup/aws/versions.tf
+++ account-setup/aws/versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = ">= 3.51.0, <= 5.100.0"    
+      version = ">= 4.47.0, <= 5.100.0"    
     }
 
     random = {
--- github/aws/versions.tf
+++ github/aws/versions.tf
@@ -6,7 +6,7 @@ terraform {
     }
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.0"
+      version = ">= 4.47.0, <= 5.100.0"
     }
   }
 }
