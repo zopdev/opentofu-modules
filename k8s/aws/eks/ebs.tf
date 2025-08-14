4a6fce8b5531d22523e107f1a651bc97b0a10b3d
arunesh-j <arunesh.j@zop.dev>
2025-08-14 14:11:23 +0530

    update ebs csi irsa role version

--- k8s/aws/eks/ebs.tf
+++ k8s/aws/eks/ebs.tf
@@ -23,6 +23,7 @@ resource "aws_eks_addon" "aws_ebs_csi_driver" {
 
 module "ebs_csi_irsa_role" {
   source = "terraform-aws-modules/iam/aws//modules/iam-role-for-service-accounts-eks"
+  version = "v5.60.0"
 
   role_name             = "${local.cluster_name}-ebs-csi"
   attach_ebs_csi_policy = true
