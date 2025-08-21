f38f700c072e9437f29ef669e9e45d81e41db6e9
Pankajvl <pankaj.vl@zop.dev>
2025-08-21 11:23:06 +0530

    removed authentication

--- k8s/aws/eks/karpenter.tf
+++ k8s/aws/eks/karpenter.tf
@@ -32,20 +32,11 @@ module "karpenter" {
 
 data "aws_partition" "current" {}
 
-provider "aws"{
-  region = "us-east-1"
-  alias  = "virginia"
-}
-data "aws_ecrpublic_authorization_token" "token"{
-  provider = aws.virginia
-}
 resource "helm_release" "karpenter" {
   count      = local.enable_karpenter ? 1 : 0
   name       = "karpenter-aws"
   namespace  = kubernetes_namespace.karpenter.metadata[0].name
   chart      = "oci://public.ecr.aws/karpenter/karpenter"
-  repository_username = data.aws_ecrpublic_authorization_token.token.user_name
-  repository_password = data.aws_ecrpublic_authorization_token.token.password
   version    = "1.6.0"
 
   values = [
