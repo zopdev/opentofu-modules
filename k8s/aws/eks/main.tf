70915e6f7885c4ee55c25b18b09b4225feb02daf
PiyushSingh-ZS <piyush.singh@zop.dev>
2025-09-01 16:19:27 +0530

    update amu

--- k8s/aws/eks/main.tf
+++ k8s/aws/eks/main.tf
@@ -72,7 +72,7 @@ module "eks" {
 
   self_managed_node_groups = {
     "${local.cluster_name}" = {
-      ami_id        = data.aws_ssm_parameter.eks_ami.value
+      ami_id        = "amazon-eks-node-al2023-x86_64-standard-1.33-v20250704"
       instance_type = var.node_config.node_type
       desired_size  = var.node_config.min_count
       min_size      = var.node_config.min_count
