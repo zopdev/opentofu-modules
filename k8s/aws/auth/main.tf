12722858b26f2d4cbc4fa38311025bfcf3223009
Pankajvl <pankaj.vl@zop.dev>
2025-09-01 12:25:07 +0530

    Added all required files for karpenter

--- k8s/aws/auth/main.tf
+++ k8s/aws/auth/main.tf
@@ -12,23 +12,32 @@ module "aws_auth" {
   manage_aws_auth_configmap = true
 
   // Organisation specific roles to be added for UI access.
-  aws_auth_roles = [
-    {
-      rolearn  = aws_iam_role.eks_cluster_admin.arn
-      username = aws_iam_role.eks_cluster_admin.name
-      groups   = ["system:masters"]
-    },
-    {
-      rolearn  = aws_iam_role.eks_cluster_viewer.arn
-      username = aws_iam_role.eks_cluster_viewer.name
-      groups   = ["cluster-viewer"]
-    },
-    {
-      rolearn  = aws_iam_role.eks_cluster_editor.arn
-      username = aws_iam_role.eks_cluster_editor.name
-      groups   = ["cluster-editor"]
-    }
-  ]
+  aws_auth_roles = concat(
+    [
+      {
+        rolearn  = aws_iam_role.eks_cluster_admin.arn
+        username = aws_iam_role.eks_cluster_admin.name
+        groups   = ["system:masters"]
+      },
+      {
+        rolearn  = aws_iam_role.eks_cluster_viewer.arn
+        username = aws_iam_role.eks_cluster_viewer.name
+        groups   = ["cluster-viewer"]
+      },
+      {
+        rolearn  = aws_iam_role.eks_cluster_editor.arn
+        username = aws_iam_role.eks_cluster_editor.name
+        groups   = ["cluster-editor"]
+      }
+    ],
+      var.karpenter_node_role_name != null ? [
+      {
+        rolearn  = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:role/${var.karpenter_node_role_name}"
+        username = "system:node:{{EC2PrivateDNSName}}"
+        groups   = ["system:bootstrappers", "system:nodes"]
+      }
+    ] : []
+  )
 
   aws_auth_users = concat(
       var.masters != null ? [
