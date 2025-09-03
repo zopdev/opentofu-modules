9219ba3aecbbc0b93f5f9e1af4bb3487da403ed8
PiyushSingh-ZS <piyush.singh@zop.dev>
2025-09-03 10:20:54 +0530

    fix ami id

--- k8s/aws/eks/main.tf
+++ k8s/aws/eks/main.tf
@@ -65,7 +65,7 @@ module "eks" {
 
   self_managed_node_groups = {
     "${local.cluster_name}" = {
-      ami_id        = "amazon-eks-node-al2023-x86_64-standard-1.33-v20250704"
+      ami_id        = "ami-000037bec3bcf510e"
       instance_type = var.node_config.node_type
       desired_size  = var.node_config.min_count
       min_size      = var.node_config.min_count
