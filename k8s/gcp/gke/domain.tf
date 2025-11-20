1883759ff784e23f570614d7a298afdf7fb5bd98
PiyushSingh-ZS <piyush.singh@zop.dev>
2025-11-20 18:00:54 +0530

    remove a record

--- k8s/gcp/gke/domain.tf
+++ k8s/gcp/gke/domain.tf
@@ -70,7 +70,6 @@ resource "google_compute_address" "lb_ip_address" {
 # Global load balancer DNS records
 resource "google_dns_record_set" "global_load_balancer_sub_domain_cname" {
   count        = local.hosted_zone != "" ? 1 : 0
-  provider     = google.shared-services
   managed_zone = data.google_dns_managed_zone.zone[0].name
   name         = "*.${local.domain_name}."
   type         = "CNAME"
@@ -79,7 +78,6 @@ resource "google_dns_record_set" "global_load_balancer_sub_domain_cname" {
 
 resource "google_dns_record_set" "global_load_balancer_top_level_domain_a" {
   count        = local.hosted_zone != "" ? 1 : 0
-  provider     = google.shared-services
   managed_zone = data.google_dns_managed_zone.zone[0].name
   name         = "${local.domain_name}."
   type         = "A"
