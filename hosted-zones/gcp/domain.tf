41c58bde505c42d12bc3d7c59d98be5fe349ff70
Pawan Murarka <pawanraj.murarka@zop.dev>
2025-12-01 19:47:45 +0530

    key updates

--- hosted-zones/gcp/domain.tf
+++ hosted-zones/gcp/domain.tf
@@ -56,7 +56,7 @@ resource "google_dns_record_set" "caa_records" {
   name         = google_dns_managed_zone.dns_zone[each.key].dns_name
   type         = "CAA"
   ttl          = 300
-  managed_zone = google_dns_managed_zone[each.key].dns_zone.name
+  managed_zone = google_dns_managed_zone.dns_zone[each.key].name
   rrdatas      = var.caa_certs
 }
 
