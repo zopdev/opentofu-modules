fa91367c86e54cb3f3976693dbb87bdf4b6379e5
Pawan Murarka <pawanraj.murarka@zop.dev>
2025-12-02 11:11:44 +0530

    updates caa record for aws and azure

--- hosted-zones/aws/domain.tf
+++ hosted-zones/aws/domain.tf
@@ -20,13 +20,21 @@ resource "google_dns_record_set" "aws_ns" {
   rrdatas = try([for ns in aws_route53_zone.main[each.key].name_servers : "${ns}."],"")
 }
 
-resource "google_dns_record_set" "caa_record" {
-  provider    = google.shared-services
-  for_each     = { for k , v in var.zones : k => v if v.add_ns_records }
-  name = "${aws_route53_zone.main[each.key].name}."
-  type = "CAA"
-  ttl  = 300
+resource "aws_route53_record" "caa_records" {
+  for_each = {
+    for k, v in var.zones :
+    k => v
+    if v.add_ns_records && length(var.caa_certs) > 0
+  }
 
-  managed_zone = data.google_dns_managed_zone.zone[0].name
-  rrdatas = try(var.caa_certs,[])
+  zone_id = aws_route53_zone.main[each.key].zone_id
+  name    = aws_route53_zone.main[each.key].name
+  type    = "CAA"
+  ttl     = 300
+
+  records = [
+    for cert in var.caa_certs : cert
+  ]
 }
+
+
--- hosted-zones/azure/domain.tf
+++ hosted-zones/azure/domain.tf
@@ -21,13 +21,25 @@ resource "google_dns_record_set" "azure_ns" {
   rrdatas = azurerm_dns_zone.zones[each.key].name_servers
 }
 
-resource "google_dns_record_set" "caa_records" {
-  provider    = google.shared-services
-  for_each     = { for k , v in var.zones : k => v if v.add_ns_records }
-  name = "${azurerm_dns_zone.zones[each.key].name}."
-  type = "CAA"
-  ttl  = 300
+resource "azurerm_dns_caa_record" "caa_records" {
+  for_each = {
+    for k, v in var.zones :
+    k => v
+    if v.add_ns_records && length(var.caa_certs) > 0
+  }
 
-  managed_zone = data.google_dns_managed_zone.gcp_zone[0].name
-  rrdatas = try(var.caa_certs,[])
+  name                = "@"
+  zone_name           = azurerm_dns_zone.zones[each.key].name
+  resource_group_name = azurerm_dns_zone.zones[each.key].resource_group_name
+  ttl                 = 300
+
+  dynamic "record" {
+    for_each = var.caa_certs
+    content {
+      flags = tonumber(split(" ", record.value)[0])
+      tag   = trim(split(" ", record.value)[1], "\"")
+      value = trim(join(" ", slice(split(" ", record.value), 2, length(split(" ", record.value)))), "\"")
+    }
+  }
 }
+
