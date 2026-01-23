063dd076942b8a0a9925ae9aeb7311125eef3b5a
jatintalgotra-zd <jatin.talgotra@zop.dev>
2026-01-23 14:29:16 +0530

    add openobserve support for aws

--- observability/aws/main.tf
+++ observability/aws/main.tf
@@ -9,11 +9,13 @@ locals {
   enable_tempo  = try(var.tempo != null ? var.tempo.enable : false, false)
   enable_cortex = try(var.cortex != null ? var.cortex.enable : false, false)
   enable_mimir  = try(var.mimir != null ? var.mimir.enable : false,false)
+  enable_openobserve = length(var.openobserve) > 0 && anytrue([for instance in var.openobserve : instance.enable])
 
   enable_ingress_loki = local.enable_loki ? (var.loki.enable_ingress != null ? var.loki.enable_ingress : false ) : false
   enable_ingress_tempo = local.enable_tempo ? (var.tempo.enable_ingress != null ? var.tempo.enable_ingress : false ) : false
   enable_ingress_mimir = local.enable_mimir ? (var.mimir.enable_ingress != null ? var.mimir.enable_ingress : false ) : false
   enable_ingress_cortex = local.enable_cortex ? (var.cortex.enable_ingress != null ? var.cortex.enable_ingress : false ) : false
+  enable_ingress_openobserve = local.enable_openobserve ? anytrue([for instance in var.openobserve : instance.enable && try(instance.enable_ingress, true)]) : false
 
   app_namespaces = {
     loki = local.enable_loki ? {
@@ -31,6 +33,10 @@ locals {
     mimir  = local.enable_mimir ? {
       services  = ["mimir-distributor:8080"]
       ingress  = local.enable_ingress_mimir
+    } : null
+    openobserve = local.enable_openobserve ? {
+      services = [for instance in var.openobserve : "${instance.name}-openobserve-standalone:5080" if instance.enable && instance.enable_ingress != false]
+      ingress  = local.enable_ingress_openobserve
     } : null
   }
 
