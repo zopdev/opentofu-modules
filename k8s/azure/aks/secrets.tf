095b4bf8a7f72b8f5fa51f14523dc43fd54e3c4e
Keerthana R <keerthana.rajasekaran@zop.dev>
2026-01-30 12:30:00 +0530

    Enable purge protection for Azure vaults

--- k8s/azure/aks/secrets.tf
+++ k8s/azure/aks/secrets.tf
@@ -24,6 +24,7 @@ resource "azurerm_key_vault" "secrets" {
   tenant_id                  = data.azurerm_client_config.current.tenant_id
   sku_name                   = "standard"
   soft_delete_retention_days = 7
+  purge_protection_enabled   = true
 
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
