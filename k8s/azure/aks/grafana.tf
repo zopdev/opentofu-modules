86303da73155108b9474b83ac4c2653156c2ca2b
Keerthana R <keerthana.rajasekaran@zop.dev>
2026-02-03 17:20:16 +0530

    Update grafana and k8s local vars in azure (#358)

--- k8s/azure/aks/grafana.tf
+++ k8s/azure/aks/grafana.tf
@@ -55,7 +55,7 @@ resource "helm_release" "grafana" {
   repository = "https://grafana.github.io/helm-charts"
 
   values = [
-    local.grafana_template[count.index]
+    local.grafana_template
   ]
   depends_on = [helm_release.prometheus]
 }
--- observability/azure/openobserve.tf
+++ observability/azure/openobserve.tf
@@ -19,25 +19,34 @@ resource "random_password" "openobserve_password" {
 }
 
 # Create template for OpenObserve values
-data "template_file" "openobserve_template" {
-  for_each = local.enable_openobserve ? { for instance in var.openobserve : instance.name => instance if instance.enable } : {}
+locals {
+  openobserve_templates = local.enable_openobserve ? {
+    for instance in var.openobserve :
+    instance.name => templatefile(
+      "${path.module}/templates/openobserve-values.yaml",
+      {
+        replica_count       = try(instance.replicaCount, 2)
+        cpu_request         = try(instance.min_cpu, "250m")
+        memory_request      = try(instance.min_memory, "1Gi")
+        cpu_limit           = try(instance.max_cpu, "1")
+        memory_limit        = try(instance.max_memory, "2Gi")
+        storage_provider    = "azure"
+        storage_region      = "auto"
+        storage_bucket_name = azurerm_storage_container.openobserve_data[instance.name].name
+        root_user_email     = "admin@zop.dev"
+        root_user_password  = random_password.openobserve_password[instance.name].result
+        storage_account     = var.storage_account
+        account_key         = var.account_access_key
 
-  template = file("${path.module}/templates/openobserve-values.yaml")
-  vars = {
-    replica_count       = try(each.value.replicaCount, 2)
-    cpu_request         = try(each.value.min_cpu, "250m")
-    memory_request      = try(each.value.min_memory, "1Gi")
-    cpu_limit           = try(each.value.max_cpu, "1")
-    memory_limit        = try(each.value.max_memory, "2Gi")
-    storage_provider    = "azure"
-    storage_region      = "auto"
-    storage_bucket_name = azurerm_storage_container.openobserve_data[each.key].name
-    root_user_email     = "admin@zop.dev"
-    root_user_password  = random_password.openobserve_password[each.key].result
-    storage_account     = var.storage_account
-    account_key         = var.account_access_key
-    additional_env_vars = length(try(each.value.env, [])) > 0 ? join("\n", [for env in each.value.env : "  - name: ${env.name}\n    value: \"${env.value}\""]) : ""
-  }
+        additional_env_vars = length(try(instance.env, [])) > 0 ? join("\n",
+          [ for env in instance.env :
+              "  - name: ${env.name}\n    value: \"${env.value}\""
+            ]
+          ) : ""
+      }
+    )
+    if instance.enable
+  } : {}
 }
 
 # Deploy OpenObserve using Helm
@@ -51,7 +60,7 @@ resource "helm_release" "openobserve" {
   namespace  = kubernetes_namespace.app_environments["openobserve"].metadata[0].name
 
   values = [
-    data.template_file.openobserve_template[each.key].rendered
+    local.openobserve_templates[each.key]
   ]
 
   depends_on = [
