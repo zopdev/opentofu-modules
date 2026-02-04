c745a52a9f3544b40d2be736b11a0c9bdac3cd5e
Gourish Hegde <gourish.hegde@zop.dev>
2026-02-04 18:40:53 +0530

    fix tempo for the gcp (#365)
    * fix mimir and tempo for the gcp
    
    * fix tempo for the gcp

--- observability/gcp/tempo.tf
+++ observability/gcp/tempo.tf
@@ -9,47 +9,47 @@ locals {
   ], {})
 
   tempo_values = local.enable_tempo ? templatefile("${path.module}/templates/tempo-values.yaml", {
-    cluster_name = local.cluster_name
-    data_bucket_name = google_storage_bucket.tempo_data[0].id
-    service_account  = google_service_account.tempo_svc_acc[0].email
-    max_receiver_msg_size = try(var.tempo.max_receiver_msg_size, "4700000")
+    cluster_name       = local.cluster_name
+    data_bucket_name   = google_storage_bucket.tempo_data[0].id
+    service_account    = google_service_account.tempo_svc_acc[0].email
+    max_receiver_msg_size = try(var.tempo.max_receiver_msg_size != null ? var.tempo.max_receiver_msg_size : "4700000", "4700000")
 
     # Ingester
-    ingester_replicas           = try(var.tempo.ingester.replicas, "1")
-    ingester_min_memory         = try(var.tempo.ingester.min_memory, "1Gi")
-    ingester_max_memory         = try(var.tempo.ingester.max_memory, "null")
-    ingester_min_cpu            = try(var.tempo.ingester.min_cpu, "null")
-    ingester_max_cpu            = try(var.tempo.ingester.max_cpu, "null")
-    ingester_autoscaling        = try(var.tempo.ingester.autoscaling, "true")
-    ingester_min_replicas       = try(var.tempo.ingester.min_replicas, "2")
-    ingester_max_replicas       = try(var.tempo.ingester.max_replicas, "30")
-    ingester_memory_utilization = try(var.tempo.ingester.memory_utilization, "")
-    ingester_cpu_utilization    = try(var.tempo.ingester.cpu_utilization, "")
+    ingester_replicas           = try(var.tempo.ingester.replicas != null ? var.tempo.ingester.replicas : "1", "1")
+    ingester_min_memory         = try(var.tempo.ingester.min_memory != null ? var.tempo.ingester.min_memory : "1Gi", "1Gi")
+    ingester_max_memory         = try(var.tempo.ingester.max_memory != null ? var.tempo.ingester.max_memory : "null", "null")
+    ingester_min_cpu            = try(var.tempo.ingester.min_cpu != null ? var.tempo.ingester.min_cpu : "null", "null")
+    ingester_max_cpu            = try(var.tempo.ingester.max_cpu != null ? var.tempo.ingester.max_cpu : "null", "null")
+    ingester_autoscaling        = try(var.tempo.ingester.autoscaling != null ? var.tempo.ingester.autoscaling : "true", "true")
+    ingester_min_replicas       = try(var.tempo.ingester.min_replicas != null ? var.tempo.ingester.min_replicas : "2", "2")
+    ingester_max_replicas       = try(var.tempo.ingester.max_replicas != null ? var.tempo.ingester.max_replicas : "30", "30")
+    ingester_memory_utilization = try(var.tempo.ingester.memory_utilization != null ? var.tempo.ingester.memory_utilization : "", "")
+    ingester_cpu_utilization    = try(var.tempo.ingester.cpu_utilization != null ? var.tempo.ingester.cpu_utilization : "", "")
 
     # Distributor
-    distributor_replicas           = try(var.tempo.distributor.replicas, "1")
-    distributor_min_memory         = try(var.tempo.distributor.min_memory, "750Mi")
-    distributor_max_memory         = try(var.tempo.distributor.max_memory, "null")
-    distributor_min_cpu            = try(var.tempo.distributor.min_cpu, "null")
-    distributor_max_cpu            = try(var.tempo.distributor.max_cpu, "null")
-    distributor_autoscaling        = try(var.tempo.distributor.autoscaling, "true")
-    distributor_min_replicas       = try(var.tempo.distributor.min_replicas, "2")
-    distributor_max_replicas       = try(var.tempo.distributor.max_replicas, "30")
-    distributor_memory_utilization = try(var.tempo.distributor.memory_utilization, "")
-    distributor_cpu_utilization    = try(var.tempo.distributor.cpu_utilization, "")
+    distributor_replicas           = try(var.tempo.distributor.replicas != null ? var.tempo.distributor.replicas : "1", "1")
+    distributor_min_memory         = try(var.tempo.distributor.min_memory != null ? var.tempo.distributor.min_memory : "750Mi", "750Mi")
+    distributor_max_memory         = try(var.tempo.distributor.max_memory != null ? var.tempo.distributor.max_memory : "null", "null")
+    distributor_min_cpu            = try(var.tempo.distributor.min_cpu != null ? var.tempo.distributor.min_cpu : "null", "null")
+    distributor_max_cpu            = try(var.tempo.distributor.max_cpu != null ? var.tempo.distributor.max_cpu : "null", "null")
+    distributor_autoscaling        = try(var.tempo.distributor.autoscaling != null ? var.tempo.distributor.autoscaling : "true", "true")
+    distributor_min_replicas       = try(var.tempo.distributor.min_replicas != null ? var.tempo.distributor.min_replicas : "2", "2")
+    distributor_max_replicas       = try(var.tempo.distributor.max_replicas != null ? var.tempo.distributor.max_replicas : "30", "30")
+    distributor_memory_utilization = try(var.tempo.distributor.memory_utilization != null ? var.tempo.distributor.memory_utilization : "", "")
+    distributor_cpu_utilization    = try(var.tempo.distributor.cpu_utilization != null ? var.tempo.distributor.cpu_utilization : "", "")
 
     # Querier & Query Frontend
-    querier_replicas        = try(var.tempo.querier.replicas, "1")
-    queryFrontend_replicas  = try(var.tempo.queryFrontend.replicas, "1")
+    querier_replicas       = try(var.tempo.querier.replicas != null ? var.tempo.querier.replicas : "1", "1")
+    queryFrontend_replicas = try(var.tempo.queryFrontend.replicas != null ? var.tempo.queryFrontend.replicas : "1", "1")
 
     # Metrics Generator
-    metrics_generator_enable                             = try(var.tempo.metrics_generator.enable, false)
-    metrics_generator_replicas                           = try(var.tempo.metrics_generator.replicas, "1")
-    metrics_generator_service_graphs_max_items           = try(var.tempo.metrics_generator.service_graphs_max_items, "30000")
-    metrics_generator_service_graphs_wait                = try(var.tempo.metrics_generator.service_graphs_wait, "30s")
-    metrics_generator_remote_write_flush_deadline        = try(var.tempo.metrics_generator.remote_write_flush_deadline, "2m")
+    metrics_generator_enable                             = try(var.tempo.metrics_generator.enable != null ? var.tempo.metrics_generator.enable : false, false)
+    metrics_generator_replicas                           = try(var.tempo.metrics_generator.replicas != null ? var.tempo.metrics_generator.replicas : "1", "1")
+    metrics_generator_service_graphs_max_items           = try(var.tempo.metrics_generator.service_graphs_max_items != null ? var.tempo.metrics_generator.service_graphs_max_items : "30000", "30000")
+    metrics_generator_service_graphs_wait                = try(var.tempo.metrics_generator.service_graphs_wait != null ? var.tempo.metrics_generator.service_graphs_wait : "30s", "30s")
+    metrics_generator_remote_write_flush_deadline        = try(var.tempo.metrics_generator.remote_write_flush_deadline != null ? var.tempo.metrics_generator.remote_write_flush_deadline : "2m", "2m")
     metrics_generator_remote_write                       = jsonencode(local.remote_write_config)
-    metrics_generator_metrics_ingestion_time_range_slack = try(var.tempo.metrics_generator.metrics_ingestion_time_range_slack, "40s")
+    metrics_generator_metrics_ingestion_time_range_slack = try(var.tempo.metrics_generator.metrics_ingestion_time_range_slack != null ? var.tempo.metrics_generator.metrics_ingestion_time_range_slack : "40s", "40s")
   }) : null
 }
 
