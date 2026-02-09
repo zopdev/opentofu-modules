816f212771787184d299bdbac4570b402d090fcc
Keerthana R <keerthana.rajasekaran@zop.dev>
2026-02-09 15:42:30 +0530

    Enable KMS encryption for S3 buckets

--- observability/aws/cortex.tf
+++ observability/aws/cortex.tf
@@ -101,6 +101,19 @@ resource "aws_s3_bucket" "cortex_data" {
   force_destroy = "true"
 }
 
+
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "cortex_data_encryption" {
+  count  = local.enable_cortex ? 1 : 0
+  bucket = aws_s3_bucket.cortex_data[0].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
 resource "kubernetes_secret" "cortex-aws-credentials" {
   count         = local.enable_cortex ? 1 : 0
   metadata {
--- observability/aws/tempo.tf
+++ observability/aws/tempo.tf
@@ -58,6 +58,17 @@ resource "aws_s3_bucket" "tempo_data" {
   force_destroy = "true"
 }
 
+resource "aws_s3_bucket_server_side_encryption_configuration" "tempo_data_encryption" {
+  count  = local.enable_tempo ? 1 : 0
+  bucket = aws_s3_bucket.tempo_data[0].id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
+
 resource "helm_release" "tempo" {
   count      = local.enable_tempo ? 1 : 0
   name       = "tempo"
