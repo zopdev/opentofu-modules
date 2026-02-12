0179c5c29f136466b0a770a9622e18ca7a5a7673
Keerthana R <keerthana.rajasekaran@zop.dev>
2026-02-12 21:41:54 +0530

    Enable KMS encryption for S3 buckets

--- object-storage/aws/main.tf
+++ object-storage/aws/main.tf
@@ -12,3 +12,15 @@ resource "aws_s3_bucket_versioning" "s3_versioning" {
     status = var.enable_versioning ? "Enabled" : "Suspended"
   }
 }
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "s3_kms_encryption" {
+  for_each = aws_s3_bucket.s3_bucket
+
+  bucket = each.value.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "aws:kms"
+    }
+  }
+}
