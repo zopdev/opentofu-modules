7c885884b9d1ce61bb0191f98f45f02766114876
keerthana-zd <keerthana.rajasekaran@zop.dev>
2026-02-23 17:28:46 +0530

    OCI : Replace community nginx with F5 nginx controller

--- k8s/oci/nginx/main.tf
+++ k8s/oci/nginx/main.tf
@@ -1,14 +1,16 @@
-resource "helm_release" "nginx_ingress" {
-  name       = var.app_name
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  namespace  = "kube-system"
-  version    = "4.8.2"
-  
-  values = [templatefile("${path.module}/templates/ingress-nginx-values.yaml", {
-    IPV4_ADDRESS        = var.lb_ip
-    prometheus_enabled  = var.prometheus_enabled
-    load_balancer_shape = var.load_balancer_shape
-    subnet_id           = var.lb_subnet_id
+# F5 NGINX Ingress Controller only (same IngressClass name "nginx", no community chart).
+# OCI LoadBalancer; optional reserved IP via annotation when lb_ip is set.
+resource "helm_release" "nginx_f5" {
+  name      = "${var.app_name}-nginx-f5"
+  chart     = "oci://ghcr.io/nginx/charts/nginx-ingress"
+  namespace = "kube-system"
+  version   = "2.4.3"
+
+  values = [templatefile("${path.module}/templates/nginx-f5-values.yaml", {
+    cluster_name         = var.app_name
+    load_balancer_shape  = var.load_balancer_shape
+    subnet_id            = var.lb_subnet_id
+    lb_ip                = var.lb_ip
+    prometheus_enabled   = var.prometheus_enabled
   })]
 }
