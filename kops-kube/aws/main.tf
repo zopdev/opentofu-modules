9ca0729f7341a9482670f0926180468ce3cecd3a
keerthana-zd <keerthana.rajasekaran@zop.dev>
2026-02-25 15:32:30 +0530

    AWS : Remove wildcard from TLS hosts

--- kops-kube/aws/main.tf
+++ kops-kube/aws/main.tf
@@ -109,7 +109,7 @@ resource "kubernetes_ingress_v1" "kops_kube_ingress" {
             service {
               name = "kops-kube"
               port {
-                number = 8000
+                number = 80
               }
             }
           }
@@ -119,7 +119,7 @@ resource "kubernetes_ingress_v1" "kops_kube_ingress" {
     }
     tls {
       secret_name = "tls-secret-replica"
-      hosts       = ["*.${var.host}"]
+      hosts       = ["kops-kube.${var.host}"]
     }
   }
   depends_on = [kubernetes_secret_v1.namespace-cert-replicator]
