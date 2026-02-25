65894bf9ed1fe0c7ae24e14150b4b21ea601e222
Keerthana R <keerthana.rajasekaran@zop.dev>
2026-02-25 07:19:57 +0530

    AWS : Replace community nginx with F5 nginx controller

--- observability/aws/main.tf
+++ observability/aws/main.tf
@@ -80,18 +80,13 @@ resource "kubernetes_ingress_v1" "service_ingress" {
   metadata {
     name      = each.value.ingress_name
     namespace = each.value.ns
-    annotations = merge(
-      {
-      "kubernetes.io/ingress.class" = "nginx"
-      },
-      each.value.ns == "mimir" && local.enable_mimir ? {
-        "nginx.ingress.kubernetes.io/auth-type"   = "basic"
-        "nginx.ingress.kubernetes.io/auth-secret"  = "mimir-basic-auth"
-        "nginx.ingress.kubernetes.io/auth-realm"  = "Authentication Required"
-      } : {}
-    )
+    annotations = each.value.ns == "mimir" && local.enable_mimir ? {
+      "nginx.org/basic-auth-secret" = "mimir-basic-auth"
+      "nginx.org/basic-auth-realm"  = "Authentication Required"
+    } : {}
   }
   spec {
+    ingress_class_name = "nginx"
     rule {
       host = each.value.domain_name
       http {
