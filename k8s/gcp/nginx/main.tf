a3e7341d6234e63efdd5cae99edd453a742a01bb
Keerthana R <keerthana.rajasekaran@zop.dev>
2026-03-09 08:04:01 +0530

    GCP : Replace community nginx with F5 nginx controller # Conflicts: #	k8s/gcp/gke/grafana.tf

--- k8s/gcp/nginx/main.tf
+++ k8s/gcp/nginx/main.tf
@@ -1,19 +1,25 @@
-resource "helm_release" "nginx_ingress" {
-  name       = var.app_name
-  repository = "https://kubernetes.github.io/ingress-nginx"
-  chart      = "ingress-nginx"
-  namespace  = "kube-system"
-  version    = "4.7.0"
+# Deletes the community IngressClass "nginx" (spec.controller is immutable, cannot be patched).
+# Trigger is static so this runs exactly once during migration and never again.
+resource "null_resource" "delete_nginx_ingressclass" {
+  triggers = {
+    controller = "nginx.org/ingress-controller"
+  }
 
-  values = [templatefile("${path.module}/templates/ingress-nginx-values.yaml", {
-    cluster_name       = var.app_name
+  provisioner "local-exec" {
+    command = "kubectl delete ingressclass nginx --ignore-not-found || true"
+  }
+}
+
+resource "helm_release" "nginx_ingress" {
+  name      = var.app_name
+  chart     = "oci://ghcr.io/nginx/charts/nginx-ingress"
+  namespace = "kube-system"
+  version   = "2.4.4"
+
+  values = [templatefile("${path.module}/templates/nginx-f5-values.yaml", {
     lb_ip              = var.lb_ip
     prometheus_enabled = var.prometheus_enabled
-  })
-  ]
+  })]
 
-  set {
-    name  = "controller.service.loadBalancerIP"
-    value = var.lb_ip
-  }
+  depends_on = [null_resource.delete_nginx_ingressclass]
 }
