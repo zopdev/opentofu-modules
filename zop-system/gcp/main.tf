064fd5b715e35e4ced197181c309a3a952621fdf
Keerthana R <keerthana.rajasekaran@zop.dev>
2026-03-09 17:08:51 +0530

    GCP : Rollback testing # Conflicts: #	k8s/gcp/gke/grafana.tf

--- zop-system/gcp/main.tf
+++ zop-system/gcp/main.tf
@@ -117,7 +117,7 @@ resource "kubernetes_ingress_v1" "kube_management_api_ingress" {
             service {
               name = "kube-management-api"
               port {
-                number = 80
+                number = 8000
               }
             }
           }
@@ -127,7 +127,7 @@ resource "kubernetes_ingress_v1" "kube_management_api_ingress" {
     }
     tls {
       secret_name = "tls-secret-replica"
-      hosts       = ["kube-management-api.${var.host}"]
+      hosts       = ["*.${var.host}"]
     }
   }
   depends_on = [kubernetes_secret_v1.namespace-cert-replicator,kubernetes_namespace.app_environments]
