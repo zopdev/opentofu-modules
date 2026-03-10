81beda27e43e2988d829ee466470d1f04b1b957e
Gourish Hegde <gourish.hegde@zop.dev>
2026-03-10 15:55:44 +0530

    add read replica support for GCP (#414)

--- k8s/gcp/namespace/configmap.tf
+++ k8s/gcp/namespace/configmap.tf
@@ -33,6 +33,8 @@ resource "kubernetes_config_map" "service_configs" {
       "DB_DIALECT"      = each.value.db_name != null ? module.sql_db[0].db_type :  each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_type : null
       "DB_HOST"         = each.value.db_name != null ? "${var.namespace}-sql.db" : each.value.datastore_configs != null ?  "${each.value.datastore_configs.name}-sql.db" : null
       "DB_PORT"         = each.value.db_name != null ? module.sql_db[0].db_port : each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_port : null
+      "DB_REPLICA_HOST" = each.value.db_name != null ? (try(var.sql_db.read_replica, false) ? "${var.namespace}-sql-replica.db" : null) : each.value.datastore_configs != null ? (try(tobool(var.sql_list[each.value.datastore_configs.name].read_replica), false) ? "${each.value.datastore_configs.name}-sql-replica.db" : null) : null
+      "DB_REPLICA_PORT" = each.value.db_name != null ? (try(var.sql_db.read_replica, false) ? module.sql_db[0].db_port : null) : each.value.datastore_configs != null ? (try(tobool(var.sql_list[each.value.datastore_configs.name].read_replica), false) ? module.sql_db_v2[each.value.datastore_configs.name].db_port : null) : null
       "REDIS_HOST"      = each.value.redis == true || each.value.local_redis == true ? (each.value.redis == true ? "${var.namespace}-redis" : "redis-master-master") : (each.value.redis_configs != null ? "${each.value.redis_configs.name}-${var.namespace}-redis" : null),
       "REDIS_PORT"      = each.value.redis == true || each.value.local_redis == true ? "6379" : (each.value.redis_configs != null ? each.value.redis_configs.port : null)
     })
@@ -55,6 +57,8 @@ resource "kubernetes_config_map" "cron_jobs_configs" {
       "DB_DIALECT"      = each.value.db_name != null ? module.sql_db[0].db_type :  each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_type : null
       "DB_HOST"         = each.value.db_name != null ? "${var.namespace}-sql.db" : each.value.datastore_configs != null ?  "${each.value.datastore_configs.name}-sql.db" : null
       "DB_PORT"         = each.value.db_name != null ? module.sql_db[0].db_port : each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_port : null
+      "DB_REPLICA_HOST" = each.value.db_name != null ? (try(var.sql_db.read_replica, false) ? "${var.namespace}-sql-replica.db" : null) : each.value.datastore_configs != null ? (try(tobool(var.sql_list[each.value.datastore_configs.name].read_replica), false) ? "${each.value.datastore_configs.name}-sql-replica.db" : null) : null
+      "DB_REPLICA_PORT" = each.value.db_name != null ? (try(var.sql_db.read_replica, false) ? module.sql_db[0].db_port : null) : each.value.datastore_configs != null ? (try(tobool(var.sql_list[each.value.datastore_configs.name].read_replica), false) ? module.sql_db_v2[each.value.datastore_configs.name].db_port : null) : null
       "REDIS_HOST"      = each.value.redis == true || each.value.local_redis == true ? (each.value.redis == true ? "${var.namespace}-redis" : "redis-master-master") : (each.value.redis_configs != null ? "${each.value.redis_configs.name}-${var.namespace}-redis" : null),
       "REDIS_PORT"      = each.value.redis == true || each.value.local_redis == true ? "6379" : (each.value.redis_configs != null ? each.value.redis_configs.port : null)
     })
