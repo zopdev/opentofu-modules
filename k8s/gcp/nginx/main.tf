5855f78ab2dd75fbb25c023ff3d2c0293e492dfe
keerthana-zd <keerthana.rajasekaran@zop.dev>
2026-03-11 22:16:55 +0530

    Revert "GCP: F5 nginx implementation"
    This reverts commit 59212a63050900e8c3cc57f406ea7cb37aa0f208.

--- k8s/gcp/nginx/main.tf
+++ k8s/gcp/nginx/main.tf
@@ -1,23 +1,19 @@
-resource "null_resource" "delete_nginx_ingressclass" {
-  triggers = {
-    controller = "nginx.org/ingress-controller"
-  }
-
-  provisioner "local-exec" {
-    command = "kubectl delete ingressclass nginx --ignore-not-found || true"
-  }
-}
-
 resource "helm_release" "nginx_ingress" {
-  name      = var.app_name
-  chart     = "oci://ghcr.io/nginx/charts/nginx-ingress"
-  namespace = "kube-system"
-  version   = "2.4.4"
+  name       = var.app_name
+  repository = "https://kubernetes.github.io/ingress-nginx"
+  chart      = "ingress-nginx"
+  namespace  = "kube-system"
+  version    = "4.7.0"
 
-  values = [templatefile("${path.module}/templates/nginx-f5-values.yaml", {
+  values = [templatefile("${path.module}/templates/ingress-nginx-values.yaml", {
+    cluster_name       = var.app_name
     lb_ip              = var.lb_ip
     prometheus_enabled = var.prometheus_enabled
-  })]
+  })
+  ]
 
-  depends_on = [null_resource.delete_nginx_ingressclass]
+  set {
+    name  = "controller.service.loadBalancerIP"
+    value = var.lb_ip
+  }
 }
