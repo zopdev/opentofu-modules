33dca11f90413a4ece41761be3fcc2fb61c8207c
jatintalgotra-zd <jatin.talgotra@zop.dev>
2026-03-13 16:53:17 +0530

    Fix null condition error for read replica config in service configmap

--- k8s/gcp/namespace/configmap.tf
+++ k8s/gcp/namespace/configmap.tf
@@ -33,8 +33,8 @@ resource "kubernetes_config_map" "service_configs" {
       "DB_DIALECT"      = each.value.db_name != null ? module.sql_db[0].db_type :  each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_type : null
       "DB_HOST"         = each.value.db_name != null ? "${var.namespace}-sql.db" : each.value.datastore_configs != null ?  "${each.value.datastore_configs.name}-sql.db" : null
       "DB_PORT"         = each.value.db_name != null ? module.sql_db[0].db_port : each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_port : null
-      "DB_REPLICA_HOST" = try(each.value.datastore_configs.read_replica, false) ? "${each.value.datastore_configs.name}-sql-replica.db" : null
-      "DB_REPLICA_PORT" = try(each.value.datastore_configs.read_replica, false) ? module.sql_db_v2[each.value.datastore_configs.name].db_port : null
+      "DB_REPLICA_HOST" = try(each.value.datastore_configs.read_replica, false) == true ? "${each.value.datastore_configs.name}-sql-replica.db" : null
+      "DB_REPLICA_PORT" = try(each.value.datastore_configs.read_replica, false) == true ? module.sql_db_v2[each.value.datastore_configs.name].db_port : null
       "REDIS_HOST"      = each.value.redis == true || each.value.local_redis == true ? (each.value.redis == true ? "${var.namespace}-redis" : "redis-master-master") : (each.value.redis_configs != null ? "${each.value.redis_configs.name}-${var.namespace}-redis" : null),
       "REDIS_PORT"      = each.value.redis == true || each.value.local_redis == true ? "6379" : (each.value.redis_configs != null ? each.value.redis_configs.port : null)
     })
@@ -57,8 +57,8 @@ resource "kubernetes_config_map" "cron_jobs_configs" {
       "DB_DIALECT"      = each.value.db_name != null ? module.sql_db[0].db_type :  each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_type : null
       "DB_HOST"         = each.value.db_name != null ? "${var.namespace}-sql.db" : each.value.datastore_configs != null ?  "${each.value.datastore_configs.name}-sql.db" : null
       "DB_PORT"         = each.value.db_name != null ? module.sql_db[0].db_port : each.value.datastore_configs != null ?  module.sql_db_v2[each.value.datastore_configs.name].db_port : null
-      "DB_REPLICA_HOST" = try(each.value.datastore_configs.read_replica, false) ? "${each.value.datastore_configs.name}-sql-replica.db" : null
-      "DB_REPLICA_PORT" = try(each.value.datastore_configs.read_replica, false) ? module.sql_db_v2[each.value.datastore_configs.name].db_port : null
+      "DB_REPLICA_HOST" = try(each.value.datastore_configs.read_replica, false) == true ? "${each.value.datastore_configs.name}-sql-replica.db" : null
+      "DB_REPLICA_PORT" = try(each.value.datastore_configs.read_replica, false) == true ? module.sql_db_v2[each.value.datastore_configs.name].db_port : null
       "REDIS_HOST"      = each.value.redis == true || each.value.local_redis == true ? (each.value.redis == true ? "${var.namespace}-redis" : "redis-master-master") : (each.value.redis_configs != null ? "${each.value.redis_configs.name}-${var.namespace}-redis" : null),
       "REDIS_PORT"      = each.value.redis == true || each.value.local_redis == true ? "6379" : (each.value.redis_configs != null ? each.value.redis_configs.port : null)
     })
