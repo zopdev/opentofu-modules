cefd727b9e829e6018a552c4fdfbff76f072b40e
jatintalgotra-zd <jatin.talgotra@zop.dev>
2026-03-24 17:10:05 +0530

    Revert "fix(azure): security hardening for Azure modules (#385)"
    This reverts commit db7aa85874cee4dcc9064fc92a4d7838ba2b5e03.

--- zop-system/azure/permissions.tf
+++ zop-system/azure/permissions.tf
@@ -150,7 +150,7 @@ resource "kubernetes_cluster_role_binding" "zop_cluster_role_binding_cluster_adm
   role_ref {
     api_group = "rbac.authorization.k8s.io"
     kind      = "ClusterRole"
-    name      = "edit"
+    name      = "cluster-admin"
   }
   subject {
     kind      = "ServiceAccount"
